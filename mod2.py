d525566bbd86b284a1fec9cd5ea552c0f605a677
jonathan rubio <jonathanr1156@gmail.com>
2023-11-06 15:54:39 -0800

    firts commit

---
+++ mod2.py
@@ -0,0 +1,27 @@
+def sqfoot():
+    house_sqfoot = ()
+
+    length = int(input('What is the length of your house in feet? '))
+    width = int(input('What is the width of your house in feet? '))
+
+    
+    print(length * width)
+
+sqfoot()
+
+
+from math import pi as p, floor as f
+
+def circumference(): 
+    from math import pi as p, floor as f
+    C = ()
+
+    r = int(input('What is the radius of your circle? '))
+
+    print(f(2 * p * r))
+
+circumference()
+
+    
+
+    
